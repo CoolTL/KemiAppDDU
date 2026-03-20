aeccff08d65464be2fc64e25cf595d7fabc84c02
NEXT15930 <fake@email.com>
2026-03-20 10:58:50 +0100

    Button sizes and no caps

--- view/ui.py
+++ view/ui.py
@@ -1,5 +1,11 @@
 from nicegui import ui
 
+ui.add_head_html('''
+<style> .q-btn { width: 60px; } </style>
+''')
+
+ui.button.default_props('no-caps')
+
 with ui.column():
     with ui.row().classes('w-full'):
         ui.button('H')
@@ -7,7 +13,42 @@ with ui.column():
         ui.button('He')
     with ui.row().classes('w-full'):
         ui.button('Li')
-        ui.button('Le')
-        ui.button('Te')
+        ui.button('Be')
+        ui.space()
+        ui.button('B')
+        ui.button('C')
+        ui.button('N')
+        ui.button('O')
+        ui.button('F')
+        ui.button('Ne')
+    with ui.row().classes('w-full'):
+        ui.button('Na')
+        ui.button('Mg')
+        ui.space()
+        ui.button('Al')
+        ui.button('Si')
+        ui.button('P')
+        ui.button('S')
+        ui.button('Cl')
+        ui.button('Ar')
+    with ui.row().classes('w-full'):
+        ui.button('K')
+        ui.button('Ca')
+        ui.button('Sc')
+        ui.button('Ti')
+        ui.button('V')
+        ui.button('Cr')
+        ui.button('Mn')
+        ui.button('Fe')
+        ui.button('Co')
+        ui.button('Ni')
+        ui.button('Cu')
+        ui.button('Zn')
+        ui.button('Ga')
+        ui.button('Ge')
+        ui.button('As')
+        ui.button('Se')
+        ui.button('Br')
+        ui.button('Kr')
 
 ui.run()
