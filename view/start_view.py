bbc2212407ab854f38b3c90b04fb2404919e612e
CoolTL <thomasliisborg@gmail.com>
2026-03-23 10:50:06 +0100

    made ui for mainscreen

---
+++ view/start_view.py
@@ -0,0 +1,20 @@
+from nicegui import ui
+
+
+class MainScreen:
+    def __init__(self):
+        self.setup_page()
+
+    def setup_page(self):
+        with ui.column().classes('w-full h-screen items-center justify-center -mt-32'):
+            ui.label('Kemidle').classes('text-8xl')
+
+            with ui.row().classes('gap-4'):
+                ui.button('quiz').classes('text-xl py-4 w-64')
+                ui.button('Periodisk System').classes('text-xl py-4 w-64')
+
+
+
+MainScreen()
+
+ui.run()
